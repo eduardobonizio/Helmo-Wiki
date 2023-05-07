1291d50e30a529103b2d982b5b7b42a837a4a2ae
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2023-05-06 21:31:30 -0300

    Base para os outros links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-/* eslint-disable react/react-in-jsx-scope */
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar from "./nav/navbar";
+import NavBar from "./elements/nav/navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Monsters from "./routes/monsters/monsters";
 // import Home from "./routes/home/home";
@@ -8,6 +7,7 @@ import PkSystem from "./routes/pk system/PkSystem";
 import Items from "./routes/items/items";
 import Item from "./routes/items/item";
 import Party from "./routes/party/party";
+import React from "react";
 
 function App() {
   return (
